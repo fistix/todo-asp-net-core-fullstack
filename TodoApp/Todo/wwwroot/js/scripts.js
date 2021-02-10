079492b05f52bea54acf6a6bd1eaec6727f6d617
mudassirmaqbool1@outlook.com <mudassirmaqbool1@outlook.com>
2021-02-10 21:13:38 +0500

    Implemented Stripe checkout view in Todo-blazor, implemented webhook(order place event handler)

---
+++ TodoApp/Todo/wwwroot/js/scripts.js
@@ -0,0 +1,7 @@
+﻿
+redirectToCheckout = function (sessionId) {
+  var stripe = Stripe('pk_test_51IIUm0KsuYyFXhSvPIN8vpVEOwJuLMLVqoqBEwPVOXO3RC2Rh8CTRs2kxWbK51SQWsR8mBvAIltGDMY0bjheLavT00NKFlsOxO');
+  stripe.redirectToCheckout({
+    sessionId: sessionId
+  });
+};
--- TodoApp/WebApi/Controllers/StripeController.cs
+++ TodoApp/WebApi/Controllers/StripeController.cs
@@ -1,26 +1,78 @@
 ﻿using Domain.Commands.Strip;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Stripe;
 using Stripe.Checkout;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fistix.Training.WebApi.Controllers
 {
   [Route("api/[controller]")]
+  //[Route("webhook")]
+
   [ApiController]
   public class StripeController : ControllerBase
   {
 
+    // You can find your endpoint's secret in your webhook settings
+    const string secret = "whsec_82Ut3MRXob9ZDzQwquSq1Eal7uXVKI4C";
+
+    //[Route("webhook")]
+    [HttpPost("index")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> Index()
+    {
+      try
+      {
+        var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+
+        var stripeEvent = EventUtility.ConstructEvent(
+          json,
+          Request.Headers["Stripe-Signature"],
+          secret
+        );
+
+        // Handle the checkout.session.completed event
+        if (stripeEvent.Type == Events.CheckoutSessionCompleted)
+        {
+          var session = stripeEvent.Data.Object as Session;
+
+          return Content("Order is Fulfilled!");
+
+          // Fulfill the purchase...
+          this.FulfillOrder(session);
+        }
+
+        return Ok();
+      }
+      catch (StripeException e)
+      {
+        return BadRequest();
+      }
+      catch(Exception ex)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+      }
+    }
+
+    private void FulfillOrder(Session session)
+    {
+      // TODO: fill me in
+      //throw new NotImplementedException();
+    }
 
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create()
     {
-      var domain = "https://localhost:5001";
+      //await Index();
+
+      var domain = "https://localhost:5200";
       var options = new SessionCreateOptions
       {
         PaymentMethodTypes = new List<string>
@@ -44,12 +96,18 @@ namespace Fistix.Training.WebApi.Controllers
                   },
                 },
         Mode = "payment",
-        SuccessUrl = domain + "/success.html",
-        CancelUrl = domain + "/cancel.html",
+        SuccessUrl = domain + "/checkoutSuccess",
+        CancelUrl = domain + "/checkoutCancel",
       };
       var service = new SessionService();
       Session session = service.Create(options);
+
+
+
       return Ok(new CreateSessionCommandResult(){ SessionId = session.Id });
     }
+
+
+
   }
 }
